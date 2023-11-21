aa0eb45fd4e34967d6e60ec929c5f2637de637a7
immengzi <immengzi@outlook.com>
2023-11-21 14:14:28 +0800

    添加项目文件。

---
+++ Recommend_Sys/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Recommend_Sys.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string _title = "Recommend System";
+    }
+}
