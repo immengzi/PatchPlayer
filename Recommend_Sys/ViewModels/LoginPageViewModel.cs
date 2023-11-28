950987efb0d74a460500b679db26ae129f62561a
immengzi <immengzi@outlook.com>
2023-11-28 16:56:13 +0800

    [Feat] Add LoginPage

---
+++ Recommend_Sys/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Recommend_Sys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Recommend_Sys.Repositories;
+
+namespace Recommend_Sys.ViewModels
+{
+    public partial class LoginPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? _username;
+
+        [ObservableProperty]
+        private string? _password;
+
+        private IUserRepository userRepository;
+
+        public LoginPageViewModel()
+        {
+            userRepository = new UserRepository();
+        }
+    }
+}
