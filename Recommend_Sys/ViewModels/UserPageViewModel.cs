82dae128eb94e674257d73ca6f564e3fef989fe8
immengzi <immengzi@outlook.com>
2023-12-02 16:33:53 +0800

    [Feat] Add User Profile

--- Recommend_Sys/ViewModels/UserPageViewModel.cs
+++ Recommend_Sys/ViewModels/UserPageViewModel.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Recommend_Sys.Models;
+using Recommend_Sys.Repositories;
 
 namespace Recommend_Sys.ViewModels
 {
     public partial class UserPageViewModel : ObservableObject
     {
+        private IUserRepository userRepository;
+
+        [ObservableProperty]
+        private string? _displayName;
+
         public UserPageViewModel()
         {
-
+            userRepository = new UserRepository();
+            LoadCurrentUserData();
+        }
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                DisplayName = $"Welcome {user.Username}!";
+            }
+            else
+            {
+                DisplayName = "Invalid user, not logged in";
+            }
         }
     }
 }
