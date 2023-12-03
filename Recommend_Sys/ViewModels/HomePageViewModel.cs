574566aae4ac0663a569d15d6ad21f7a3574ee23
immengzi <immengzi@outlook.com>
2023-12-03 14:03:55 +0800

    [Fix] Enable Home's search

--- Recommend_Sys/ViewModels/HomePageViewModel.cs
+++ Recommend_Sys/ViewModels/HomePageViewModel.cs
@@ -1,55 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
+using System.Xml.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using HandyControl.Controls;
 using Recommend_Sys.Models;
 using Recommend_Sys.Services;
+using MessageBox = HandyControl.Controls.MessageBox;
 
 namespace Recommend_Sys.ViewModels
 {
     public partial class HomePageViewModel : ObservableObject
     {
         [ObservableProperty]
-        public List<Song> _songs;
+        private ObservableCollection<Song> _songs;
 
         [ObservableProperty]
         private string? _text;
 
-        [ObservableProperty]
-        private string? _name;
-
-        [ObservableProperty]
-        private string? _singer;
-
-        [ObservableProperty]
-        private string? _album;
-
         [RelayCommand]
-        public async void SearchSong()
+        public async Task SearchSongAsync()
         {
-            // 使用Task.Run在后台线程执行GetSongAsync
-            var songs = await Task.Run(() => GetSong.GetSongAsync(songName: _text));
-
-            // 在UI线程上更新属性
-            Application.Current.Dispatcher.Invoke(() =>
+            try
             {
-                foreach (var song in songs)
+                var searchSongs = await Task.Run(() => GetSong.GetSongAsync(songName: _text));
+
+                // 在 UI 线程上更新属性
+                Application.Current.Dispatcher.Invoke(() =>
                 {
-                    Name = song.name;
-                    Singer = song.artist_name;
-                    Album = song.album_name;
-                }
-            });
+                    Songs.Clear();
+
+                    foreach (var searchSong in searchSongs)
+                    {
+                        Songs.Add(new Song()
+                        {
+                            name = searchSong.name,
+                            artist_name = searchSong.artist_name,
+                            album_name = searchSong.album_name,
+                            id = searchSong.id,
+                            url = searchSong.url
+                        });
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         public HomePageViewModel()
         {
-
+            _songs = new ObservableCollection<Song>();
         }
     }
 }
